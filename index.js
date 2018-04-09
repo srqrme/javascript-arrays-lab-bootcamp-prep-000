7a7f0ae62ea5b5427a0bfa4cb7b5635ff320469a
Sharmaine Requerme <srqrme@gmail.com>
2018-04-09 14:56:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"]
 
-function destructivelyAppendKitten(name)
+function destructivelyAppendKitten(name) {
   kittens.push(name)
+}
 end
